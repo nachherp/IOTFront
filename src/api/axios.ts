964f0fd3faba6b8ffea3b36db0816180a20c240d
IRD22-JIHP <ih43108@gmail.com>
2025-01-28 06:55:00 -0500

    el pepe

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Cambia esto a la URL de tu backend
+  baseURL: 'http://localhost:5000',
 });
 
 export default api;
